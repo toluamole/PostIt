f3ffa5094a667beaa04e032479c3f4ae7aed881c
Amole Toluwanimi <31034421+toluamole@users.noreply.github.com>
2020-11-27 12:05:23 +0100

    [Feat]Forgot password and reset

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,6 +4,8 @@ const users = require('../controllers/users');
 const router = Router();
 
 router.post('/api/user/signup', users.signup_post);
-router.post('/api/user/login', users.login_post)
+router.post('/api/user/login', users.login_post);
+router.post('/api/user/forgotPassword', users.sendResetLink);
+router.post('/reset_password/:token', users.resetPassword);
 
 module.exports = router;
